1016aa750450819bec8e46b315c03e0ebe6d3c0f
timjerman <tim.jerman@gmail.com>
2014-11-05 18:22:23 +0100

    initial version of game model + context

---
+++ celtraJackpotPlayer/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace celtraJackpotPlayer.Models
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public string GameLocation { get; set; }
+        public int Machines { get; set; }
+        public int Pulls { get; set; }
+        public int NumOfPlays { get; set; }
+
+        public int NumOfSections { get; set; }
+        public int[] Sections { get; set; }
+        public float[,] Probabilities { get; set; }
+        public int[,] SectionsScore { get; set; }
+        public int[,] SectionsCount { get; set; }
+        public bool isConstant { get; set; }
+
+        public int[] Score { get; set; }
+    }
+}
---
+++ celtraJackpotPlayer/Models/GameContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace celtraJackpotPlayer.Models
+{
+    public class GameContext : DbContext
+    {
+        public GameContext() : base("GameContext")
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }s
+    }
+}
